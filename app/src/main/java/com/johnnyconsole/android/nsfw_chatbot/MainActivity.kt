8d58f720fb64287f6ac2ff29b2f15ebaecf9c153
Johnny Console <johnny.console@hotmail.com>
2026-07-12 19:55:09 -0400

    Add ChatMessage class to define a message object

--- app/src/main/java/com/johnnyconsole/android/nsfw_chatbot/MainActivity.kt
+++ app/src/main/java/com/johnnyconsole/android/nsfw_chatbot/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.johnnyconsole.android.nsfw_chatbot.adapters.ChatMessageAdapter
 import com.johnnyconsole.android.nsfw_chatbot.databinding.ActivityMainBinding
+import com.johnnyconsole.android.nsfw_chatbot.objects.ChatMessage
+import com.johnnyconsole.android.nsfw_chatbot.objects.MessageSender
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +26,16 @@ class MainActivity : AppCompatActivity() {
                 v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
                 insets
             }
+
+            val array = arrayOf(ChatMessage(MessageSender.SENDER_BOT, "From the Bot!"),
+                ChatMessage(MessageSender.SENDER_USER, "Fron the User!"),
+                ChatMessage(MessageSender.SENDER_BOT, "This is a really long message from the bot and it should wrap to the next like, allowing us to test the width of the message box. Hopefully it will work as I intend it to :)"),
+                ChatMessage(MessageSender.SENDER_USER, "This is a really long message from the user and it should wrap to the next like, allowing us to test the width of the message box. Hopefully it will work as I intend it to :)"),
+            )
+
+            rvChatMessages.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvChatMessages.adapter = ChatMessageAdapter(this@MainActivity, array)
+
         }
     }
 }
---
+++ app/src/main/java/com/johnnyconsole/android/nsfw_chatbot/objects/ChatMessage.kt
@@ -0,0 +1,3 @@
+package com.johnnyconsole.android.nsfw_chatbot.objects
+
+class ChatMessage(val sender: MessageSender, val text: String)
